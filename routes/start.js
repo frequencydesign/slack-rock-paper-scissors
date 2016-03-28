cc1b36fb51cddcee4cbf04ec7cd897d934879c79
Mahdi Tehrani <mahdi@frequencydesign.com>
2016-03-27 23:20:10 -0700

    changing username after a game is started

--- routes/start.js
+++ routes/start.js
@@ -1,9 +1,19 @@
 exports.post = function(req, res, next) {
     //console.log('Start route.');
     //res.send("Res.Send Start.");
+    /*
+    UPON POSTING
+    Send a request for an opponent
+    Consider a timer for accepting of a match
+    when a match is accepted, create a match in the DB so that there is something to hold the answers and compare winners
+
+
+     */
     res.json({
-        "username": "ghost-bot",
-        "icon_emoji": ":ghost:",
-        "text": "BOO!"
+        "username": "outgoing-rps",
+        //"icon_emoji": ":ghost:",
+        "text": "You started a game!"
     });
+
+    next();
 };
