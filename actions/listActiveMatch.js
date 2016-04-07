994bc0ff2c98229822d350e6f3091389e3cbddfe
Mahdi Tehrani <mahdi@frequencydesign.com>
2016-04-07 11:40:00 -0700

    changing passed element

--- actions/listActiveMatch.js
+++ actions/listActiveMatch.js
@@ -3,6 +3,7 @@ var dbActions = require('./../persist.js');
 
 function listActiveMatch(data) {
     var theMatchData = JSON.parse(data);
+    var newMatchID = theMatchData.matchName;
     console.log("listActiveMatch theMatchData.active " + theMatchData.active);
     if (theMatchData.active != 1) {
         console.log("No active match. Setting up new match.")
@@ -13,6 +14,7 @@ function listActiveMatch(data) {
 
 function closeMatch(data) {
     var theMatchData = JSON.parse(data);
+    var newMatchID = theMatchData.matchName;
     console.log(theMatchData.active);
     theMatchData.active = 0;
     console.log(theMatchData.active);
